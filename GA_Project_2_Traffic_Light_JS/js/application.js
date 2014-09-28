c91b6c0eadaca02ed22cd2a9761b2954c439e792
Susing Yin <susingyin@gmail.com>
2014-09-28 19:46:56 -0400

    Project 3 (JS Traffic Light) - Version 2

--- GA_Project_2_Traffic_Light_JS/js/application.js
+++ GA_Project_2_Traffic_Light_JS/js/application.js
@@ -1,1 +1,32 @@
-alert("hello world")
+function stopClick (){
+  var goLight = document.getElementById('goLight');
+  var cautionLight = document.getElementById('cautionLight');
+  
+  goLight.style.backgroundColor = "black";
+  cautionLight.style.backgroundColor = "black";
+  
+  var light = document.getElementById('stopLight');
+  light.style.backgroundColor = "red";
+}
+
+function cautionClick (){
+  var goLight = document.getElementById('goLight');
+  var stopLight = document.getElementById('stopLight');
+  
+  goLight.style.backgroundColor = "black";
+  stopLight.style.backgroundColor = "black";
+  
+  var light = document.getElementById('cautionLight');
+  light.style.backgroundColor = "yellow";
+}
+
+function goClick (){
+  var cautionLight = document.getElementById('cautionLight');
+  var stopLight = document.getElementById('stopLight');
+  
+  cautionLight.style.backgroundColor = "black";
+  stopLight.style.backgroundColor = "black";
+  
+  var light = document.getElementById('goLight');
+  light.style.backgroundColor = "green";
+}
